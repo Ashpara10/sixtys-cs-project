a419afde76b979bb675682686ea658ef6d9d8600
Ashwin <ashwinparande1156610c@gmail.com>
2022-08-10 12:27:23 +0530

    Addbook updated

--- AddBook.py
+++ AddBook.py
@@ -1,9 +1,39 @@
 import tkinter as tk
-def log():
-    print("id :"+bookInfo1.get()+"\n"+"title :"+bookInfo2.get()+"\n"+"author :"+bookInfo3.get()+"\n"+"status :"+bookInfo4.get())
+import tkinter.ttk as ttk
+from tkinter import messagebox
+import pymysql
+def registerBook():
+    bid = bookInfo1.get()
+    title = bookInfo2.get()
+    author = bookInfo3.get()
+    status = bookInfo4.get()
+    status = status.lower()
+    mypass = "ashpara21092005" #use your own password
+    mydatabase="db" #The database name
+    con = pymysql.connect(host="localhost",user="root",password=mypass,database=mydatabase)
+    #root is the username here
+    cur = con.cursor()
+    insertBooks = "insert into books values('"+bid+"','"+title+"','"+author+"','"+status+"')"
+    try:
+        cur.execute(insertBooks)
+        con.commit()
+        messagebox.showinfo('Success',"Book added successfully")
+    except:
+        messagebox.showinfo("Error","Can't add data into Database")
+    
+    print(bid)
+    print(title)
+    print(author)
+    print(status)
+
+
+    
 
 def addBook():
  global bookInfo1,bookInfo2,bookInfo3,bookInfo4 
+
+ 
+
  root = tk.Tk()
  root.title("Library")
  root.minsize(width=400,height=400)
@@ -11,47 +41,47 @@ def addBook():
 
 
  Canvas1 = tk.Canvas(root)   
- Canvas1.config(bg="#ff6e40")
+#  Canvas1.config(bg="#ff6e40")
  Canvas1.pack(expand=True,fill=tk.BOTH)
      
- headingFrame1 = tk.Frame(root,bg="#FFBB00",bd=5)
+ headingFrame1 = tk.Frame(root,bd=5)
  headingFrame1.place(relx=0.25,rely=0.1,relwidth=0.5,relheight=0.13) 
- headingLabel = tk.Label(headingFrame1, text="Add Books", bg='black', fg='white', font=('Courier',15))
+ headingLabel = tk.Label(headingFrame1, text="Add Books", font=('Courier',15))
  headingLabel.place(relx=0,rely=0, relwidth=1, relheight=1) 
  labelFrame = tk.Frame(root,bg='black')
  labelFrame.place(relx=0.1,rely=0.4,relwidth=0.8,relheight=0.4)
      
  # Book ID
- lb1 = tk.Label(labelFrame,text="Book ID : ", bg='black', fg='white')
+ lb1 = ttk.Label(labelFrame,text="Book ID : ")
  lb1.place(relx=0.05,rely=0.2, relheight=0.08)
      
- bookInfo1 = tk.Entry(labelFrame)
+ bookInfo1 = ttk.Entry(labelFrame)
  bookInfo1.place(relx=0.3,rely=0.2, relwidth=0.62, relheight=0.08)
 #  bookInfo1.pack(padx=10,pady=10)
      
  # Title
- lb2 = tk.Label(labelFrame,text="Title : ", bg='black', fg='white')
+ lb2 = ttk.Label(labelFrame,text="Title : ")
  lb2.place(relx=0.05,rely=0.35, relheight=0.08)
 
- bookInfo2 = tk.Entry(labelFrame)
+ bookInfo2 = ttk.Entry(labelFrame)
  bookInfo2.place(relx=0.3,rely=0.35, relwidth=0.62, relheight=0.08)
      
  # Book Author
- lb3 = tk.Label(labelFrame,text="Author : ", bg='black', fg='white')
+ lb3 = tk.Label(labelFrame,text="Author : ")
  lb3.place(relx=0.05,rely=0.50, relheight=0.08)
      
  bookInfo3 = tk.Entry(labelFrame)
  bookInfo3.place(relx=0.3,rely=0.50, relwidth=0.62, relheight=0.08)
      
  # Book Status
- lb4 = tk.Label(labelFrame,text="Status(Avail/issued) : ", bg='black', fg='white')
+ lb4 = ttk.Label(labelFrame,text="Status(Avail/issued) : ")
  lb4.place(relx=0.05,rely=0.65, relheight=0.08)
      
- bookInfo4 = tk.Entry(labelFrame)
+ bookInfo4 = ttk.Entry(labelFrame)
  bookInfo4.place(relx=0.3,rely=0.65, relwidth=0.62, relheight=0.08)
      
  #Submit Button
- SubmitBtn = tk.Button(root,text="SUBMIT",bg='#d1ccc0', fg='black',command=log)
+ SubmitBtn = ttk.Button(root,text="SUBMIT",command=registerBook)
  SubmitBtn.place(relx=0.28,rely=0.9, relwidth=0.18,relheight=0.08)
  
  quitBtn = tk.Button(root,text="Quit",bg='#f7f1e3', fg='black', command=root.destroy)
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import tkinter as tk
+import tkinter.ttk as ttk
 from  AddBook import addBook
 import pymysql
 
 root = tk.Tk()
-root.title("Quandale Dingle")
+root.title("Dataflair")
 root.minsize(width=400,height=400)
 root.geometry("600x500")
 same=True
@@ -15,7 +16,7 @@ n=0.70
 
 mypass = "ashpara21092005" #use your own password
 mydatabase="db" #The database name
-con = pymysql.connect (host="localhost",user="root",password=mypass,database=mydatabase)
+con = pymysql.connect(host="localhost",user="root",password=mypass,database=mydatabase)
 #root is the username here
 cur = con.cursor()
 # --------------------------
@@ -23,24 +24,25 @@ cur = con.cursor()
 print(cur.execute("SELECT title FROM books"))
 
 
-headingFrame1 = tk.Frame(root,bg="#212121",bd=5)
+headingFrame1 = ttk.Frame(root,padding=10)
 headingFrame1.place(relx=0.2,rely=0.05,relwidth=0.6,relheight=0.16)
-headingLabel = tk.Label(headingFrame1, text="Welcome to \n Sixty's Library", bg='#212121', fg='white', font=('Courier',15))
+headingLabel = ttk.Label(headingFrame1, text="Welcome to \n Sixty's Library", font=('Courier',15),border=50)
 headingLabel.place(relx=0,rely=0, relwidth=1, relheight=1)
 
-btn1 = tk.Button(root,text="Add Book Details",bg='black', fg='white', command=addBook)
+
+btn1 = ttk.Button(root,text="Add Book Details",command=addBook)
 btn1.place(relx=0.28,rely=0.4, relwidth=0.45,relheight=0.1)
     
-btn2 = tk.Button(root,text="Delete Book",bg='black', fg='white')
+btn2 = ttk.Button(root,text="Delete Book",)
 btn2.place(relx=0.28,rely=0.5, relwidth=0.45,relheight=0.1)
     
-btn3 = tk.Button(root,text="View Book List",bg='black', fg='white', )
+btn3 = ttk.Button(root,text="View Book List" )
 btn3.place(relx=0.28,rely=0.6, relwidth=0.45,relheight=0.1)
     
-btn4 = tk.Button(root,text="Issue Book to Student",bg='black', fg='white', )
+btn4 = ttk.Button(root,text="Issue Book to Student" )
 btn4.place(relx=0.28,rely=0.7, relwidth=0.45,relheight=0.1)
     
-btn5 = tk.Button(root,text="Return Book",bg='black', fg='white', )
+btn5 = ttk.Button(root,text="Return Book")
 btn5.place(relx=0.28,rely=0.8, relwidth=0.45,relheight=0.1)
 
 root.mainloop()
